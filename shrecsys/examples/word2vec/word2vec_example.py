f2f24f109d3778d6740e65fa47d10207f8fa94b5
August <xuezhengyin@sohu-inc.com>
2018-03-12 10:47:53 +0800

    topic2vec

--- shrecsys/examples/word2vec/word2vec_example.py
+++ shrecsys/examples/word2vec/word2vec_example.py
@@ -1,18 +1,19 @@
 # -*- coding:utf-8 -*-
 import sys
-sys.path.append("/data/app/xuezhengyin/app/shrecsys")
+#sys.path.append("/data/app/xuezhengyin/app/shrecsys")
 from shrecsys.models.models import Model
 from shrecsys.models.topic2vec.topic2vec import Topic2vec
 from shrecsys.preprocessing.corpus import Corpus
 from shrecsys.preprocessing.videoTokenizer import VideoTokenizer, load_videos_topics
 from shrecsys.preprocessing.viewTokenizer import ViewTokenizer
 from shrecsys.util.fileSystemUtil import FileSystemUtil
-#ROOT = "/data/app/xuezhengyin/app/shrecsys/data/word2vec/data"
-ROOT = "../../../data/word2vec"
-IDF_PATH = ROOT + "/videos_IDF.txt"
-VIDEO_TITLE = ROOT + "/videos_title"
-VIEW_SEQS = ROOT + "/view_seqs"
-PREDICT_PATH = ROOT + "/predict_videos_title"
+TRAIN_ROOT = "../../../data/word2vec"
+PREDICT_ROOT = "./data_online"
+#ROOT = "../../../data/word2vec"
+IDF_PATH = "/videos_IDF.txt"
+VIDEO_TITLE = "/videos_title"
+VIEW_SEQS = "/view_seqs"
+PREDICT_PATH = "/predict_videos_title"
 #EMBED_SIZE = 300
 #NUM_SAMPLED = 64
 #CONTEXT_SIZE = 5
@@ -21,7 +22,7 @@ PREDICT_PATH = ROOT + "/predict_videos_title"
 #EPOCH = 20
 #BATCH_SIZE = 300
 #TOP_K = 100
-#MIN_CNT = 3
+#MIN_CNT = 5
 
 
 
@@ -37,8 +38,8 @@ MIN_CNT = 0
 fstool = FileSystemUtil()
 def preprecessing(view_seqs,video_num):
     corpus = Corpus()
-    corpus.load_idf(IDF_PATH)
-    corpus.calcu_videos_tfidf(VIDEO_TITLE,video_num)
+    corpus.load_idf(TRAIN_ROOT + IDF_PATH)
+    corpus.calcu_videos_tfidf(TRAIN_ROOT + VIDEO_TITLE,video_num)
     videos_tfidf = corpus.get_videos_tfidf()
     videoTokenzier = VideoTokenizer(videos_tfidf)
     #videoTokenzier.load_videos_topics(TFIDF_PATH,videos_topics)
@@ -47,7 +48,7 @@ def preprecessing(view_seqs,video_num):
     videoTokenzier.videos_intersection(viewTokenizer.get_videos_index())
     viewTokenizer.view_to_index_topics_seqs(videoTokenzier.get_videos_topics_index())
     corpus.clear("videos_title videos_tfidf")
-    fstool.save_obj(corpus, ROOT, "corpus")
+    fstool.save_obj(corpus, TRAIN_ROOT, "corpus")
     return viewTokenizer, videoTokenzier
 
 if __name__=="__main__":
@@ -56,15 +57,19 @@ if __name__=="__main__":
         print("videos_num: the number of the videos title!")
         exit(-1)
     videos_num = int(sys.argv[1])
-    input_view = open(VIEW_SEQS)
+    input_view = open(TRAIN_ROOT + VIEW_SEQS)
     view_seqs = [line.strip().split() for line in input_view.readlines()]
     viewTokenzier, videoTokenzier = preprecessing(view_seqs, videos_num)
     topics_size = videoTokenzier.get_topics_size()
     videos_size = videoTokenzier.get_videos_size()
     topic2vec = Topic2vec(topics_size + 1, videos_size + 1, EMBED_SIZE, NUM_SAMPLED, CONTEXT_SIZE)
-    save_config = {"model_path": ROOT, "save_iter": ITER}
+    save_config = {"model_path": TRAIN_ROOT, "save_iter": ITER}
     topic2vec.config(save_config)
     model = Model(topic2vec, EPOCH, LEARN_RATING, BATCH_SIZE)
-    model.fit(viewTokenzier.get_view_topics_index(), viewTokenzier.get_view_index())
-    videoTokenzier.clear("videos_topics videos_index videos_topics_index")
-    fstool.save_obj(videoTokenzier, ROOT, "videoTokenzier")
+    input = viewTokenzier.get_view_topics_index()
+    output = viewTokenzier.get_view_index()
+    videoTokenzier.clear("videos_topics videos_topics_index videos_index")
+    fstool.save_obj(videoTokenzier, TRAIN_ROOT, "videoTokenzier")
+    del viewTokenzier
+    del videoTokenzier
+    model.fit(input, output)
