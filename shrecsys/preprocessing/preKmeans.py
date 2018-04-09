1be4b677502c59bb0d0d388925a3dba87ec9793d
AugustGgGreen <33374197+AugustGgGreen@users.noreply.github.com>
2018-04-09 20:07:16 +0800

    kmeans

--- shrecsys/preprocessing/preKmeans.py
+++ shrecsys/preprocessing/preKmeans.py
@@ -9,6 +9,7 @@ def load_sen2vec_embedding(SEN2VEC, view_videos_index):
     videos_index = dict()
     videos_embedding = []
     index = 0
+    index_ = 0
     while line:
         points = line.split(' ')
         id = points[0]
@@ -18,7 +19,8 @@ def load_sen2vec_embedding(SEN2VEC, view_videos_index):
             videos_embedding.append(embedding)
             index += 1
         line = input.readline().strip()
-        if index % 100000 == 0:
+        index_ += 1
+        if index_ % 100000 == 0:
             logging.info("build sen2vec embedding, index: {}".format(index))
     logging.info("generate embedding of sen2vec success! embedding size: {}".format(len(videos_index)))
     return videos_embedding, videos_index
