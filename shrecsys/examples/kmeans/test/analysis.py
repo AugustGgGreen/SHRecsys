55790d916d367a368cda85c018ad52ecea20bc22
AugustGgGreen <33374197+AugustGgGreen@users.noreply.github.com>
2018-07-02 11:15:40 +0800

    v1.2.0

--- shrecsys/examples/kmeans/test/analysis.py
+++ shrecsys/examples/kmeans/test/analysis.py
@@ -4,7 +4,9 @@ def output_video_tilte(clusters_videos):
     videoDao = VideoDao()
     file = open("../../../data/Kmeans/title.txt","w")
     for cluster in clusters_videos:
+        file.write("cluster id "+ str(cluster) + "\n")
         videos_val = clusters_videos[cluster]
         for video in videos_val:
             title = videoDao.get_video_title(video)
-            file.write(str(video) +": "+ title + "  value:" + str(videos_val[video]))
+            keyword = videoDao.get_video_key_words(video)
+            file.write(str(video) +": :"+ str(title)+ "value: :" + str(videos_val[video]) + "keyword: :"+ str(keyword) + "\n")
